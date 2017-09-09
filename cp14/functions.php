d546f31a43f357c7ffb259587dd84de97419ff3b
Mannuel Victor Limeira <victor@Mannuels-MacBook-Air.local>
2017-09-09 17:01:07 -0300

    refactoring database connection.

--- cp14/functions.php
+++ cp14/functions.php
@@ -7,19 +7,6 @@ function dumpp($value)
     echo "</pre>";
 }
 
-function connectDatabase()
-{
-    try {
-
-        return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');
-
-    } catch (PDOException $e) {
-
-        die($e->getMessage());
-
-    }
-}
-
 function FetchAllTasks($pdo)
 {
     $statement = $pdo->prepare('select * from todos');
--- cp14/index.php
+++ cp14/index.php
@@ -1,9 +1,10 @@
 <?php
 
 require 'functions.php';
+require 'database/Connection.php';
 require 'Task.php';
 
-$pdo = connectDatabase();
+$pdo = Connection::make();
 
 $tasks = FetchAllTasks($pdo);
 
