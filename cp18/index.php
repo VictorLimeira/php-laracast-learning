8315706754dde27a7a4ee64ccda3bdb67c9a3ffb
Mannuel Victor Limeira <victor@Mannuels-MacBook-Air.local>
2017-10-14 11:46:26 -0300

    Initiate index with basic class

--- cp18/index.php
+++ cp18/index.php
@@ -1,1 +1,25 @@
-<?php  ?>
+<?php
+
+/**
+ * Post Class
+ */
+class Post
+{
+    public $title;
+    public $published;
+
+    public function __construct($title, $published)
+    {
+        $this->title = $title;
+        $this->published = $published;
+    }
+}
+
+$posts = [
+    new Post('Title one', true),
+    new Post('Title two', true),
+    new Post('Title three', true),
+    new Post('Title four', false)
+];
+
+var_dump($posts);
